70d9696448bfc65b92666924fef9d90d2cf3714f
YJ <cvcv_cv@naver.com>
2024-01-09 17:27:26 +0900

    델리게이트 연습 2

---
+++ 14. Delegate_Practice2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _14._Delegate_Practice2
+{
+	internal class Program
+	{
+		public class Player
+		{
+			public void Jump() { Console.WriteLine("플레이어가 점프~"); }
+			public void Dash() { Console.WriteLine("플레이어가 대쉬~"); }
+		}
+
+		public class Button
+		{
+			public Action OnClick;
+			public virtual void Click()
+			{
+				if (OnClick != null) { OnClick(); }
+			}
+		}
+		static void Main(string[] args)
+		{
+			Player player = new Player();
+			player.Jump();
+
+			Button jumpButton = new Button();
+			jumpButton.OnClick = player.Jump;
+
+			jumpButton.Click();
+
+			Button dashButton = new Button();
+			dashButton.OnClick = player.Dash;
+
+			dashButton.Click();
+		}
+	}
+}
