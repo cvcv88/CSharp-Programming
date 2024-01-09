3bca2a09fbdafd13df368cca4d79f6cb97d0fcbc
YJ <cvcv_cv@naver.com>
2024-01-09 17:16:23 +0900

    docs : 인터페이스
    인터페이스 : 클래스가 해당 행동을 진행할 수 있음을 표현하며 각 클래스에서 반응을 구현

---
+++ 10. Interface/Architecture.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._Interface
+{
+	internal class Architecture
+	{
+		/* * * * * * *  
+		 * 추상클래스와 인터페이스 (*자주 물어봄!!!! 이해하기~)
+         * 
+         * 인터페이스는 추상클래스의 일종으로 특징이 동일함
+         * 함수에 대한 선언만 정의하고 이를 포함하는 클래스에서 구체화하여 사용
+         * 하지만, 추상클래스와 인터페이스를 통해 얻는 효과는 다르며 다른 역할을 수행함
+         * 개발자는 인터페이스와 추상클래스 중 더욱 상황에 적합한 것으로 구현해야 함
+		 * * * * * * */
+
+		public abstract class EnterableObject
+		{
+			public string key;
+			public abstract void Enter();
+		}
+		// 역할이 비슷함, 반드시 포함하는 클래스에서 해당 기능을 구현하게 해야 함
+		// 상속할건지 ... 그 행동을 할건지
+		public interface IEnterable
+		{
+			// public string key; // 변수 선언 불가
+			public void Enter();
+		}
+
+		// <추상클래스와 인터페이스>
+		// 공통점 : 함수에 대한 선언만 정의하고 이를 포함하는 클래스에서 구체화하여 사용
+		// 추상클래스는 상속, 인터페이스는 포함.
+		// 차이점 : 추상클래스 - 변수, 함수의 구현 포함 가능 / 다중상속 불가
+		//          인터페이스 - 변수, 함수의 구현 포함 불가 / 다중포함 가능
+
+		// 추상클래스 (A Is B 관계) '=. 설계도
+		// 상속 관계인 경우, 자식클래스가 부모클래스의 ^하위^분류인 경우(차 - 승용차, 승합차, SUV ... )
+		// 상속을 통해 얻을 수 있는 효과를 얻을 수 있음(업, 다운캐스팅, 코드 재사용 등)
+		// 부모클래스의 기능을 통해 자식클래스의 기능을 확장하는 경우 사용
+
+		// 인터페이스 (A Can B 관계) '=. 계약서 
+		// 행동 포함인 경우, 클래스가 해당 행동을 할 수 있는 경우(플레이어 - move jump ...)
+		// 인터페이스를 사용하는 모든 클래스와 상호작용이 가능한 효과를 얻을 수 있음
+		// 인터페이스에 정의된 함수들을 클래스의 목적에 맞게 기능을 구현하는 경우 사용
+
+		public interface IEnterable1
+		{
+			void Enter();
+		}
+
+		public abstract class Building : IEnterable1
+		{
+			public void Enter()
+			{
+				Console.WriteLine("건물에 들어갑니다.");
+			}
+		}
+
+		// 은행은 건물이다 : Ok, 상속관계가 적합
+		// 업캐스팅 효과, 많은 기능 구현 그대로 사용 가능
+		public class Bank : Building
+		{
+
+		}
+
+		// 차는 들어갈 수 있다 : Ok, 인터페이스가 적합
+		// 코드 재사용의 효과를 받는게 오히려 역효과, 업캐스팅했을때 이상해지기도 함.
+		public class Car : IEnterable1
+		{
+			public void Enter()
+			{
+				Console.WriteLine("차 문을 열고 들어갑니다.");
+			}
+		}
+	}
+}
---
+++ 10. Interface/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace _10._Interface
+{
+	internal class Program
+	{
+		/* * * * * * *  
+		 * 인터페이스 (Interface) : 어떻게 상호작용할 것인지 정의해놓은 것.
+		 * UI : User Interface, 게임이랑 유저가 상호작용할 수 있도록 도와주는 것
+		 * Interface : a랑 b랑 상호작용할 때 도와주는 것.
+         * 
+         * 인터페이스는 맴버를 가질 수 있지만 직접 구현하지 않음 단지 정의만을 가짐
+         * 인터페이스를 가지는 클래스에서 반드시 인터페이스의 정의를 구현해야함
+         * 이를 반대로 표현하자면 인터페이스를 포함하는 클래스는
+         * 반드시 인터페이스의 구성 요소들을 구현했다는 것을 보장함
+         * Can-a 관계 : 클래스가 해당 행동을 할 수 있는 경우 적합함
+         * 상속과 인터페이스를 동시에 사용 가능하지만 상속이 맨 앞에 와야한다.
+		 * * * * * * */
+
+		// <인터페이스 정의>
+		// 일반적으로 인터페이스의 이름은 I로 시작함
+		// 인터페이스의 함수는 직접 구현하지 않고 정의만 진행
+
+		public interface IEnterable
+		{
+			public void Enter();
+		}
+
+		public interface IOpenable
+		{
+			public void Open();
+		}
+
+
+		// <인터페이스 포함>
+		// 상속처럼 정의한 인터페이스를 클래스 : 뒤에 선언하여 사용
+		// 인터페이스를 포함하는 경우 반드시 인터페이스에서 정의한 함수를 구현해야 함
+		// 인터페이스는 여러개 포함 가능
+
+		/*		public class IOpenable
+				{
+					public virtual void Open()
+					{
+						Console.WriteLine("열립니다.");
+					}
+					public virtual void Close()
+					{
+
+					}
+				}
+
+				public class IEnterable
+				{
+					public virtual void Enter()
+					{
+						Console.WriteLine("들어갑니다.");
+						// 기타 등등 기능 ...
+					}
+				}*/
+
+		public class Box : IOpenable, IDamagalbe
+		{
+			public void Open()
+			{
+				Console.WriteLine("박스를 엽니다.");
+				Console.WriteLine("아이템이 온전하게 나옵니다");
+			}
+			public void TakeHit(int damage)
+			{
+				Console.WriteLine("아이템이 내구도가 닳아서 나왔습니다.");
+			}
+		}
+		public class Door : IOpenable, IEnterable, IDamagalbe // 한번에 두 인터페이스 가능
+		{
+			public void Open()
+			{
+				Console.WriteLine("문을 엽니다."); ;
+			}
+			public void Enter()
+			{
+				Console.WriteLine("문에 들어갑니다"); ;
+			}
+			public void TakeHit(int damage)
+			{
+				Console.WriteLine("문을 부셨다.");
+			}
+		}
+
+		public class Town : IEnterable
+		{
+			public void Enter()
+			{
+				Console.WriteLine("마을에 진입합니다.");
+			}
+		}
+
+		public interface IDamagalbe
+		{
+			public void TakeHit(int damage);
+		}
+
+
+		// <인터페이스 사용>
+		// 인터페이스를 이용하여 기능을 구현할 경우
+		// 클래스의 상속관계와 무관하게 행동의 가능여부로 상호작용 가능
+		public class Player
+		{
+			public void Open(IOpenable openable)
+			{
+				Console.WriteLine("플레이어가 대상을 열기 시도합니다.");
+				openable.Open();
+			}
+			public void Enter(IEnterable enterable)
+			{
+				Console.WriteLine("플레이어가 대상에 들어가기 시도합니다.");
+				enterable.Enter();
+			}
+			public void Attack(IDamagalbe damagable)
+			{
+				Console.WriteLine("플레이어가 대상을 공격하기 시도합니다.");
+				damagable.TakeHit(10);
+			}
+		}
+		public class Monster : IDamagalbe
+		{
+			public int hp;
+			public void TakeHit(int damage)
+			{
+				hp -= damage;
+				Console.WriteLine("몬스터가 데미지를 받았다.");
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Player player = new Player();
+			Box box = new Box();
+			Town town = new Town();
+
+			player.Open(box);
+			player.Enter(town);
+
+			Door door = new Door();
+
+			player.Open(door);
+			player.Enter(door);
+
+			Monster monster = new Monster();
+			player.Attack(monster);
+			player.Attack(door);
+			player.Attack(box);
+		}
+	}
+}
