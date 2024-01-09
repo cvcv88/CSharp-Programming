051bfde181b4a5eeffc098b2ab4c566c3b8ff52b
YJ <cvcv_cv@naver.com>
2024-01-09 17:26:29 +0900

    델리게이트 연습

---
+++ 14. Delegate_Practice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _14._Delegate_Practice1
+{
+	public class Program
+	{
+		public static double Add(double left, double right) { return left + right; }
+		public static double Minus(double left, double right) { return left - right; }
+		public static double Mult(double left, double right) { return left * right; }
+		public static double Div(double left, double right) { return left / right; }
+
+		public class Calculator
+		{
+			public delegate double Delegate(double left, double right);
+			Delegate delegate1;
+
+			public double left;
+			public double right;
+
+			public void SetCommand(double left, char oper, double right)
+			{
+				// 계산 금지
+				this.left = left;
+				this.right = right;
+				switch (oper)
+				{
+					case '+':
+						delegate1 = Add;
+						break;
+					case '-':
+						delegate1 = Minus;
+						break;
+					case '*':
+						delegate1 = Mult;
+						break;
+					case '/':
+						delegate1 = Div;
+						break;
+				}
+			}
+
+			public double Equal()
+			{
+				// 조건문 쓰기 금지
+				double result = delegate1(left, right);
+				return result;
+			}
+		}
+		static void Main1(string[] args)
+		{
+			Calculator cal = new Calculator();
+			cal.SetCommand(2.5, '+', 3.2);
+			double result = cal.Equal();
+			Console.WriteLine(result);
+		}
+
+	}
+}
---
+++ 14. Delegate_Practice/Solve.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _14._Delegate_Practice1.Program.Calculator;
+
+namespace _14._Delegate_Practice
+{
+	internal class Solve
+	{
+		public class Calculator
+		{
+			public static double Plus(double left, double right) { return left + right; }
+			public static double Minus(double left, double right) { return left - right; }
+			public static double Multi(double left, double right) { return left * right; }
+			public static double Div(double left, double right) { return left / right; }
+
+			double left;
+			double right;
+			Func<double, double, double> calc;
+
+			//public delegate double Delegate(double left, double right);
+			//Delegate delegate1;
+
+			public void SetCommand(double left, char oper, double right)
+			{
+				// 계산금지
+				this.left = left;
+				this.right = right;
+
+				switch (oper)
+				{
+					case '+':
+						this.calc = Plus;
+						break;
+					case '-':
+						this.calc = Minus;
+						break;
+					case '*':
+						this.calc = Multi;
+						break;
+					case '/':
+						this.calc = Div;
+						break;
+				}
+			}
+
+			public double Equal()
+			{
+				// 조건문 쓰기 금지
+				return calc(left, right);
+			}
+		}
+
+		public static void Main()
+		{
+			Calculator cal = new Calculator();
+			cal.SetCommand(2.5, '+', 3.2);
+			double result = cal.Equal();
+			Console.WriteLine(result);
+			// 결과가 5.7이 나오도록 구현
+		}
+
+	}
+}
