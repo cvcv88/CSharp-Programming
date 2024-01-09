1b08b62537ad142ada4ead2b63c4e8e9e736cdeb
YJ <cvcv_cv@naver.com>
2024-01-09 16:55:34 +0900

    docs : 객체지향프로그래밍
    프로그램 설계방법론이자 개념의 일종
    프로그램을 서로 상호작용하는 객체를 기본 단위로 구성하는 방식

---
+++ 09. OOP/Inheritance.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._OOP
+{
+	public class Inheritance
+	{
+		/* * * * * * *  
+		 * 상속 (Inheritance)
+         *
+         * 부모클래스의 모든 기능을 가지는 자식클래스를 설계하는 방법
+         * is-a 관계 : 부모클래스가 자식클래스를 포함하는 상위개념일 경우 상속관계가 적합함
+		 * * * * * * */
+
+		// <상속>
+		// 부모클래스를 상속하는 자식클래스에게 부모클래스의 모든 기능을 부여
+		// class 자식클래스이름 : 부모클래스이름
+
+		public class Monster
+		{
+			public string name;
+			public int hp;
+
+			public Monster()
+			{
+				Console.WriteLine("몬스터 생성자");
+			}
+
+			public void Move()
+			{
+				Console.WriteLine($"{name} 이/가 움직입니다.");
+			}
+
+			public void Die()
+			{
+				// 경험치 떨어뜨리기
+			}
+
+			public void TakeHit(int damage)
+			{
+				hp -= damage;
+				Console.WriteLine($"{name} 이/가 데미지를 받아 체력이 {hp} 가 되었습니다.");
+			}
+		}
+		public class Dragon : Monster
+		{
+			public Dragon()
+			{
+				Console.WriteLine("드래곤 생성자");
+				name = "지드래곤";
+				hp = 100;
+			}
+
+			public void Breath()
+			{
+				Console.WriteLine($"{name} 이/가 브레스를 뿜습니다.");
+			}
+
+			public void Reflect()
+			{
+				Console.WriteLine("반격");
+			}
+		}
+
+		public class Slime : Monster
+		{
+			public Slime()
+			{
+				name = "슬라임";
+				hp = 25;
+			}
+			public void Split()
+			{
+				Console.WriteLine($"{name} 이/가 분열합니다.");
+			}
+		}
+
+		public class Orc : Monster
+		{
+			public Orc()
+			{
+				name = "오크";
+				hp = 50;
+			}
+			public void Rage()
+			{
+				Console.WriteLine($"{name} 이/가 분노합니다.");
+			}
+		}
+
+		public class AttackRange : Monster
+		{
+			public float range;
+			// 마우스 클릭 구현
+			public void SetTarget()
+			{
+
+			}
+		}
+
+		public class Hero
+		{
+			int damage = 3;
+
+			// 상속 하지 않았을 때는 몬스터 새로 생길때마다 만들어줘야 한다..
+			/*public void Attack(Dragon dragon){ }
+			 *public void Attack(Slime slime){ }
+			 *public void Attack(Orc orc){ }
+			*/
+
+			// 부모클래스를 매개변수로 주면서 자식클래스까지 같이 포함시킨다. = 업캐스팅
+			public void Attack(Monster monster)
+			{
+				monster.TakeHit(damage);
+
+				// 드래곤이면 반격도 해야함 (다운캐스팅 사용하는 예제)
+				if (monster is Dragon)
+				{
+					Dragon dragon = (Dragon)monster;
+					dragon.Reflect();
+				}
+			}
+		}
+		public static void Main1(string[] args)
+		{
+			Dragon dragon = new Dragon();
+			dragon.TakeHit(10);
+			dragon.Breath();
+
+			// Main에서 초기화할 떄 형태
+			/*dragon.name = "용";
+			dragon.hp = 100;*/
+
+			Slime slime = new Slime();
+			slime.TakeHit(10);
+			slime.Split();
+
+			Orc orc = new Orc();
+			orc.TakeHit(10);
+			orc.Rage();
+
+			Console.WriteLine();
+
+			// 업캐스팅 : 자식클래스는 부모클래스 자료형으로 묵시적 형변환 가능
+			// 자식클래스를 부모클래스에 보관(묵시적)
+			// Monster monster이지만 자식클래스가 부모클래스 자료형으로 자동적으로 형변환된다.
+			// 자식클래스가 부모클래스의 자료형으로 묵시적 형변환이 되면서, 부모의 기능을 사용할 수 있는것.
+			// 반대로 부모클래스 변수 = new 자식클래스();일때 자식클래스의 기능은 수행불가다!
+
+			Hero hero = new Hero();
+			hero.Attack(dragon);
+			hero.Attack(slime);
+			hero.Attack(orc);
+
+			Console.WriteLine();
+
+			Monster monster1 = new Dragon();
+			Monster monster2 = new Slime();
+
+			monster1.TakeHit(10); ;
+			// monster1.Breath();	// error, 자식만의 기능은 수행불가, 다운캐스팅 필요
+
+			monster2.TakeHit(10); ;
+			// monster2.spilt();	// error
+
+			Console.WriteLine();
+
+			// 다운캐스팅
+			// 부모클래스를 자식클래스에 보관(명시적)
+			Dragon dra = (Dragon)monster1; // 100% 인 경우에만 진행(명시적) , 오류는 아님
+										   // 업캐스팅한 객체를 다시 자식 클래스 타입의 객체로 되돌리는 것을 목적으로 한다.(복구)
+										   // 위에서 Monster monster1 = new Dradon();으로 선언했기에 monster1을 (Dragon)으로 형변환 가능.
+										   // 형변환이 가능하다는게 100%면 사용해도 되지만,, 혹시모르니 아래 방법 쓰기.
+
+			// Slime sli = (Slime)monster1; // 형변환이 불가능한 경우 오류
+
+			if (monster1 is Dragon) // 인스턴스(monster1)가 dragon인지 is 키워드 사용
+			{
+				Console.WriteLine("monster1은 지드래곤입니다.");
+				Dragon d = (Dragon)monster1;
+			}
+			else
+			{
+				Console.WriteLine("monster1은 지드래곤이 아닙니다.");
+			}
+
+			if (monster2 is Dragon) // 인스턴스(monster2)가 dragon인지
+			{
+				Console.WriteLine("monster2는 지드래곤입니다.");
+				Dragon d = (Dragon)monster2;
+			}
+			else
+			{
+				Console.WriteLine("monster2은 지드래곤이 아닙니다.");
+			}
+
+			// as : 형변환이 가능하면 바로 바꿔주고, 불가능하면 null 반환.
+			Dragon asDragon = monster1 as Dragon;
+			Slime asSlime = monster2 as Slime;
+		}
+
+
+		// <상속 사용의미 1>
+		// 상속을 진행하는 경우 부모클래스의 소스가 자식클래스에서 모두 적용됨
+		// 부모클래스와 자식클래스의 상속관계가 적합한 경우,
+		// 부모클래스에서의 기능 구현이 자식클래스에서도 어울림
+		class Fruit
+		{
+			// 부모클래스에서 기능을 구현할 경우 모든 부모를 상속하는 자식클래스에 기능을 구현하는 효과
+		}
+
+		class Apple : Fruit
+		{
+			// 자식클래스에서 자식클래스만의 기능을 구현
+		}
+
+
+		// <상속 사용의미 2>
+		// 업캐스팅을 통해 자식클래스는 부모클래스로 형변환이 가능함
+		// 자식클래스는 부모클래스를 요구하는 곳에서 동일한 기능을 수행할 수 있음
+		class Parent
+		{
+			public void Func() { }
+		}
+		class Child1 : Parent { }
+		class Child2 : Parent { }
+		class Child3 : Parent { }
+
+		void UseParent(Parent parent) { parent.Func(); }
+		void Main2()
+		{
+			Child1 child1 = new Child1();
+			Child2 child2 = new Child2();
+			Child3 child3 = new Child3();
+
+			UseParent(child1);
+			UseParent(child2);
+			UseParent(child3);
+		}
+	}
+}
+
+
---
+++ 09. OOP/Polymorphism.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._OOP
+{
+	internal class Polymorphism
+	{
+		/* * * * * * *  
+		 * 다형성 (Polymorphism)
+         *
+         * 객체의 속성이나 기능이 상황에 따라 여러가지 형태를 가질 수 있는 성질
+		 * * * * * * */
+
+		// <다형성>
+		// 부모클래스의 멤버를 자식클래스가 상황에 따라 여러가지 형태를 가질 수 있는 성질
+		// 동일한 부모클래스를 상속받아도 자식클래스에서 각각 다른 반응을 보이는 것.(수치, 데이터 등)
+		// 몬스터가 공격받았을 때 몬스터 종류마다 다르게 반응하는것(상속과 아주 유사함)
+		class Car
+		{
+			protected string name;
+			protected int speed;
+
+			public void Move()
+			{
+				Console.WriteLine($"{name} 이/가 {speed} 의 속도로 이동합니다.");
+			}
+		}
+
+		class Truck : Car
+		{
+			public Truck()
+			{
+				name = "트럭";
+				speed = 30;
+			}
+		}
+
+		class SportCar : Car
+		{
+			public SportCar()
+			{
+				name = "스포츠카";
+				speed = 100;
+			}
+		}
+
+		void Main1()
+		{
+			Car car1 = new Truck();
+			Car car2 = new SportCar();
+
+			car1.Move();    // 트럭 이/가 30 의 속도로 이동합니다.
+			car2.Move();    // 스포츠카 이/가 100 의 속도로 이동합니다.
+		}
+
+
+		// 함수(기능)는 조금 다르다.
+		// <가상함수와 오버라이딩>
+		// 가상함수   : 부모클래스의 함수 중 자식클래스에 의해 재정의 할 수 있는 함수를 지정
+		// virtual 함수, 부모 클래스에 virtual을 붙여서 재정의한다. 재정의 허용해준다는 표시.
+
+		// 오버라이딩 : 부모클래스의 가상함수를 같은 함수이름과 같은 매개변수로 재정의하여 자식만의 반응을 구현
+		// override, 가상함수가 아닌 함수를 오버라이드 할 수 없고,
+		// 가상함수가 맞다면 그 함수를 대신해서 내 함수를 덮어씌운다.
+
+		// virtual - override 는 세트
+
+		// 원리(CS 지식) 가상함수 테이블
+
+		class Skill
+		{
+			protected float coolTime;
+			public virtual void Execute()       // 가상함수 virtual 함수
+			{
+				Console.WriteLine("스킬 재사용 대기시간을 진행시킴");
+			}
+		}
+
+		class FireBall : Skill
+		{
+			public FireBall()
+			{
+				coolTime = 3f;
+			}
+			public override void Execute()      // 오버라이딩 override, 재정의, 덮어쓴다.
+			{
+				base.Execute();      // base : 부모클래스를 가리킴
+									 //Console.WriteLine("스킬 재사용 대기시간을 진행시킴");
+				Console.WriteLine("전방에 화염구를 날림");
+			}
+		}
+
+		class Heal1 : Skill
+		{
+			public Heal1()
+			{
+				coolTime = 15f;
+			}
+			public override void Execute()
+			{
+				base.Execute();
+				// Console.WriteLine("스킬 재사용 대기시간을 진행시킴");
+				Console.WriteLine("상처를 치료한다.");
+			}
+
+		}
+
+		static void Main2()
+		{
+			Skill fireBall = new FireBall();
+			fireBall.Execute();     // 자식클래스의 함수가 호출됨
+									// 스킬 재사용 대기시간을 진행시킴
+									// 전방에 화명구를 날림
+
+			Skill heal = new Heal1();
+			heal.Execute();
+
+			Console.WriteLine();
+			// 왜 위아래 결과가 다를까?
+			// 위는 fireBall과 heal이 Skill로 만들어졌고, 각 자식클래스가 override 하지 않은 상태여서
+			// Skill의 execute가 실행되었다.
+
+			// 아래는 fireBall과 heal이 각 상속 자식 클래스로 만들어졌기때문에 override 하지 않아도
+			// 각 FireBall.Execute(), Heal.Execute()가 실행된다!!!
+			// 그러나 아래같은 상황은 잘 쓰지 않는다. 자식이 온갖 스킬을 가지고 있어야 하기 때문에.
+			/* 내가 지금 아직 어떤 캐릭터를 선택할지 모르는 상황에서 player는 일단 스킬을 쓴다라는 기능을
+			 * 만들어야한다. 근데 그 과정에서 내가 특정 한 스킬로 스킬을 저장한다면, 나는 계속 그 스킬밖에
+			 * 쓰지 못할것!! 그렇기 때문에 스킬이라는 큰 틀을 우선 잡고나서 virtual override로 세부적으로
+			 * 들어가는 것이다
+			 * 아니면 abstract 추상화로 일단 UseSkill 만들어놓고나서 아래에서 무조건 세부조건 설정하게
+			 * 할수도 있음~~~!!!
+			 */
+
+			FireBall fireBall1 = new FireBall();
+			fireBall1.Execute();
+
+			Heal heal1 = new Heal();
+			heal1.Execute();
+		}
+
+
+		// <다형성 사용의미 1>
+		// 새로운 클래스를 추가하거나 확장할 때 기존 코드에 영향을 최소화함
+
+		// 플레이어에 skill을 넣어두었기때문에 새로운 skill이 생겨도 문제없다.
+		// 어렵땅
+		class Player
+		{
+			Skill skill;
+
+			public void SetSkill(Skill skill)
+			{
+				this.skill = skill;
+			}
+
+			public void UseSkill()
+			{
+				skill.Execute();
+			}
+		}
+
+		class Heal : Skill      // 새로운 클래스를 만들 때 기존의 소스를 수정하지 않아도 됨
+		{
+			public override void Execute()
+			{
+				base.Execute();
+				Console.WriteLine("아군의 체력을 회복함");
+			}
+		}
+
+
+		// <다형성 사용의미 2>
+		// 클래스 간의 의존성을 줄여 확장성은 높임
+		class SkillContents : Skill { }     // 프로그램의 확장을 위해 상위클래스를 상속하는 클래스를 개발
+
+
+	}
+}
